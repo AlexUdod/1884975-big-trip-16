34a7e02c9136b9eed1e80100baad2468f2c4df35
alex <bagatur8@gmail.com>
2021-12-01 11:58:21 +0200

    add all templates

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import {createSiteMenuTemplate} from './view/travel-menu-view';
 import {createSiteFiltersTemplate} from './view/travel-filters-view';
 import {createSiteSortTemplate} from './view/travel-sort-view';
+import {createSiteListTemplate} from './view/travel-list-points-view';
+import {createSiteFormTemplate} from './view/travel-form-view';
+import {createSiteFormEditorTemplate} from './view/travel-form-editor-view';
+import {createSiteDestinationPointTemplate} from './view/trave-destination-point-view';
 import {renderTemplate, renderPosition} from './render';
 
 const siteHeader = document.querySelector('.page-header');
@@ -16,3 +20,19 @@ renderTemplate(siteMenuContainer, createSiteMenuTemplate(), renderPosition.BEFOR
 renderTemplate(siteFiltersContainer, createSiteFiltersTemplate(), renderPosition.BEFOREEND);
 
 renderTemplate(siteSortContainer, createSiteSortTemplate(), renderPosition.BEFOREEND);
+
+renderTemplate(siteSortContainer, createSiteListTemplate(), renderPosition.BEFOREEND);
+
+const siteListPoints = siteSortContainer.querySelector('.trip-events__list');
+
+renderTemplate(siteListPoints, createSiteFormTemplate(), renderPosition.BEFOREEND);
+
+renderTemplate(siteListPoints, createSiteFormEditorTemplate(), renderPosition.AFTERBEGIN);
+
+const destinationPointsAmount = 3;
+
+for (let i = 0; i < destinationPointsAmount; i++) {
+  renderTemplate(siteListPoints, createSiteDestinationPointTemplate(), renderPosition.BEFOREEND);
+}
+
+
